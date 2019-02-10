93417e77ba503331ec778c543d67dbd2b36fd713
sartorphil <40663995+sartorphil@users.noreply.github.com>
2019-02-10 21:32:09 +0100

    Add files via upload

--- Plot4.R
+++ Plot4.R
@@ -7,7 +7,7 @@ dataFile <- "household_power_consumption.txt"
 data <- read.table(dataFile, header=TRUE, sep=";", na.strings = "?", stringsAsFactors=FALSE, dec=".")
 ## subsetting data to dates 2007-02-01 and 2007-02-02
 subpower <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
-## convert the Date and Time variables to Date/Time classes
+## convert the Date and Time variables to Date/Time classes and subset the data
 datetime <- strptime(paste(subpower$Date, subpower$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 GlobalActivePower <- as.numeric(subpower$Global_active_power)
 Voltage <- as.numeric(subpower$Voltage)
